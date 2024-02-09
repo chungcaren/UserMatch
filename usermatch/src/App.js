c1c40eb52b108e42565d73e4e4949e962b624a5a
Caren Chung <chungcaren2@gmail.com>
2024-02-09 11:36:45 -0500

    fixed post methods + css changes

--- usermatch/src/App.js
+++ usermatch/src/App.js
@@ -17,10 +17,16 @@ function App() {
     const longitude = position.coords.longitude;
     setLocation({ latitude, longitude });
     
-    fetch('http://localhost:3000/updateLocation')
-      .then((res) => res.json())
-      .then((data) => setLocation(data))
-      .catch((error) => console.error("Error fetching location: ", error));
+    fetch('http://localhost:3000/updateLocation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        latitude: latitude,
+        longitude: longitude,
+      }),
+    }).catch((error) => console.error("Error fetching location: ", error));
   }
 
   function error() {
@@ -29,59 +35,82 @@ function App() {
 
   return (
     <main className="app-main">
-      <div className="location-button">
-        <button onClick={getLocation}>Allow Location</button>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <div className="location-button">
+          <button onClick={getLocation}>Allow Location</button>
+        </div>
       </div>
-      <div>
-        <profileInput/>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <header>Fill out your user profile!</header>
       </div>
-  </main>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+        <div className="user-profile">
+          <ProfileInput/>
+          <SubmitProfile/>
+        </div>
+      </div>
+    </main>
   );
 }
 
-function profileInput() {
-  const setProfile = Array.from(document.querySelectorAll('#userProfile input')).reduce((acc, input) => ({...acc, [input.id]: input.value }), {});
-  
-  fetch('http://localhost:3000/userprofile')
-      .then((res) => res.json())
-      .then((data) => setProfile(data))
-      .catch((error) => console.error("Error fetching profile: ", error));
+function ProfileInput() {
 
   return(
     <div className="profile-input">
     <form id="userProfile">
+      <label for="username">Username:</label><br/>
+      <input type="text" id="username" name="username"/><br/>
+      <label for="password">Password:</label><br/>
+      <input type="text" id="password" name="password"/><br/>
       <label for="name">Name:</label><br/>
       <input type="text" id="name" name="name"/><br/>
       <label for="age">Age:</label><br/>
       <input type="text" id="age" age="age"/><br/>
-
-      <label for="interests">Interests/Hobbies:</label><br/>
-      <input type="checkbox" id="interest1" name="interest1" value="Traveling"/>
-      <label for="interest1"> Traveling</label><br/>
-      <input type="checkbox" id="interest2" name="interest2" value="Working Out"/>
-      <label for="interest2"> Working Out</label><br/>
-      <input type="checkbox" id="interest3" name="interest3" value="Eating"/>
-      <label for="interest3"> Eating</label><br/>
-      <input type="checkbox" id="interest4" name="interest4" value="Art"/>
-      <label for="interest4"> Art</label><br/>
-      <input type="checkbox" id="interest5" name="interest5" value="Gaming"/>
-      <label for="interest5"> Gaming</label><br/>
-      
-      <label for="travelSpots">Travel Spots:</label><br/>
-      <input type="checkbox" id="country1" name="country1" value="Spain"/>
-      <label for="country1"> Spain</label><br/>
-      <input type="checkbox" id="country2" name="country2" value="France"/>
-      <label for="country2"> France</label><br/>
-      <input type="checkbox" id="country3" name="country3" value="Japan"/>
-      <label for="country3"> Japan</label><br/>
-      <input type="checkbox" id="country4" name="country4" value="Korea"/>
-      <label for="country4"> Korea</label><br/>
-      <input type="checkbox" id="country5" name="country5" value="Switzerland"/>
-      <label for="country5"> Switzerland</label><br/>
-
-      <input type="submit" value="Submit" onClick/>
+      <label for="interests">Interests:</label><br/>
+      <input type="text" id="interests" name="interests"/><br/>
+      <label for="travel_spots">Travel Spots:</label><br/>
+      <input type="text" id="travel_spots" name="travel_spots"/><br/>
+      <label for="hobbies">Hobbies:</label><br/>
+      <input type="text" id="hobbies" name="hobbies"/><br/>
+      <label for="working_out">Working Out:</label><br/>
+      <input type="text" id="working_out" name="working_out"/><br/>
     </form>
   </div>
+  );
+}
+
+function SubmitProfile() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get('username');
+    const password = formData.get('password');
+    const name = formData.get('name');
+    const age = formData.get('age');
+    const interests = formData.get('interests');
+    const travel_spots = formData.get('travel_spots');
+    const hobbies = formData.get('hobbies');
+    const working_out = formData.get('working_out');
+
+    fetch('http://localhost:3000/userprofile', { 
+      method: 'POST', 
+      headers: { 
+        'Content-Type': 'application/json', 
+      }, 
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        name: name,
+        age: age,
+        interests: interests,
+        travel_spots: travel_spots,
+        hobbies: hobbies,
+        working_out: working_out,
+      }), 
+    }).catch((error) => console.error("Error fetching profile: ", error));
+  }
+  return (
+    <input type="submit" value="Submit" onClick={handleClick}/>
   );
 }
 

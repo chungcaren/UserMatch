332f5725111665f699fac9accb59f730703aeb1f
Caren Chung <chungcaren2@gmail.com>
2024-04-05 10:02:23 -0400

    show/hide elements for navigation

--- usermatch/src/App.js
+++ usermatch/src/App.js
@@ -3,6 +3,10 @@ import './App.css'; // Import the CSS file for styling
 import Modal from "./Components/Modal";
 
 function App() {
+
+  const [showForm, setShowForm] = useState(true);
+  const [showMatches, setShowMatches] = useState(false);
+  
   const [location, setLocation] = useState(null);
 
   function getLocation() {
@@ -57,6 +61,9 @@ function App() {
     const form = useRef(null);
 
     const handleSubmit = (e) => {
+      setShowForm((showForm) => !showForm)
+      setShowMatches((showMatches) => !showMatches)
+
       e.preventDefault();
       const formData = new FormData(form.current);
 
@@ -93,8 +100,10 @@ function App() {
         }),
       }).catch((error) => console.error("Error fetching profile: ", error));
     };
-
+    
     return (
+      <div className="user-profile">
+        <h1>Fill out your user profile!</h1>
       <div className="profile-input">
         <form id="userProfile" ref={form} onSubmit={handleSubmit}>
           <label htmlFor="username">Username:</label><br />
@@ -120,6 +129,7 @@ function App() {
           <input type="submit" value="Submit" />
         </form>
       </div>
+      </div>
     );
   }
 
@@ -135,58 +145,61 @@ function App() {
     return false;
   }
 
+  function RevealUserMatches() {
+    return(
+      <div className="nearby-users">
+      <h1>Matches Near You:</h1>
+      <div className="match1">
+        <center><img src="https://openclipart.org/download/247324/abstract-user-flat-1.svg" 
+          width={50} 
+          height={50} 
+          alt="usericon"/>
+          <p>{buddies[0] !== undefined ? buddies[0].name : 'NAME'}</p></center>
+        <p>Age: {buddies[0] !== undefined ? buddies[0].age : ''}</p>
+        <p>Interests: {buddies[0] !== undefined ? buddies[0].interests : ''}</p>
+        <p>Travel Spots: {buddies[0] !== undefined ? buddies[0].travel_spots : ''}</p>
+        <p>Hobbies: {buddies[0] !== undefined ? buddies[0].hobbies : ''}</p>
+        <p>Working Out: {buddies[0] !== undefined ? String(buddies[0].working_out) : ''}</p>
+        <p>Distance: With 25 miles</p> 
+      </div>
+      <div className="match2">
+        <center><img src="https://openclipart.org/download/247324/abstract-user-flat-1.svg" 
+          width={50} 
+          height={50} 
+          alt="usericon"/>
+          <p>{buddies[1] !== undefined ? buddies[1].name : 'NAME'}</p></center>
+        <p>Age: {buddies[1] !== undefined ? buddies[1].age : ''}</p>
+        <p>Interests: {buddies[1] !== undefined ? buddies[1].interests : ''}</p>
+        <p>Travel Spots: {buddies[1] !== undefined ? buddies[1].travel_spots : ''}</p>
+        <p>Hobbies: {buddies[1] !== undefined ? buddies[1].hobbies : ''}</p>
+        <p>Working Out: {buddies[1] !== undefined ? String(buddies[1].working_out) : ''}</p>
+        <p>Distance: Within 25 miles</p>
+      </div>
+      <div className="match3">
+        <center><img src="https://openclipart.org/download/247324/abstract-user-flat-1.svg" 
+          width={50} 
+          height={50} 
+          alt="usericon"/>
+          <p>{buddies[2] !== undefined ? buddies[2].name : 'NAME'}</p></center>
+        <p>Age: {buddies[2] !== undefined ? buddies[2].age : ''}</p>
+        <p>Interests: {buddies[2] !== undefined ? buddies[2].interests : ''}</p>
+        <p>Travel Spots: {buddies[2] !== undefined ? buddies[2].travel_spots : ''}</p>
+        <p>Hobbies: {buddies[2] !== undefined ? buddies[2].hobbies : ''}</p>
+        <p>Working Out: {buddies[2] !== undefined ? String(buddies[2].working_out) : ''}</p>
+        <p>Distance: Within 25 miles</p>
+      </div>
+    </div>
+    )
+  }
+
   return (
     <main className="app-main">
       {openModal && <Modal closeModal={setOpenModal} getLocation={getLocation} />}
       <div className="left-section">
-        <div className="user-profile">
-          <h1>Fill out your user profile!</h1>
-          <ProfileInput />
-        </div>
+        {showForm && <ProfileInput />}
       </div>
       <div className="right-section">
-        <div className="nearby-users">
-          <h1>Matches Near You:</h1>
-          <div className="match1">
-            <center><img src="https://openclipart.org/download/247324/abstract-user-flat-1.svg" 
-              width={50} 
-              height={50} 
-              alt="usericon"/>
-              <p>{buddies[0] !== undefined ? buddies[0].name : 'NAME'}</p></center>
-            <p>Age: {buddies[0] !== undefined ? buddies[0].age : ''}</p>
-            <p>Interests: {buddies[0] !== undefined ? buddies[0].interests : ''}</p>
-            <p>Travel Spots: {buddies[0] !== undefined ? buddies[0].travel_spots : ''}</p>
-            <p>Hobbies: {buddies[0] !== undefined ? buddies[0].hobbies : ''}</p>
-            <p>Working Out: {buddies[0] !== undefined ? String(buddies[0].working_out) : ''}</p>
-            <p>Distance: With 25 miles</p> 
-          </div>
-          <div className="match2">
-            <center><img src="https://openclipart.org/download/247324/abstract-user-flat-1.svg" 
-              width={50} 
-              height={50} 
-              alt="usericon"/>
-              <p>{buddies[1] !== undefined ? buddies[1].name : 'NAME'}</p></center>
-            <p>Age: {buddies[1] !== undefined ? buddies[1].age : ''}</p>
-            <p>Interests: {buddies[1] !== undefined ? buddies[1].interests : ''}</p>
-            <p>Travel Spots: {buddies[1] !== undefined ? buddies[1].travel_spots : ''}</p>
-            <p>Hobbies: {buddies[1] !== undefined ? buddies[1].hobbies : ''}</p>
-            <p>Working Out: {buddies[1] !== undefined ? String(buddies[1].working_out) : ''}</p>
-            <p>Distance: Within 25 miles</p>
-          </div>
-          <div className="match3">
-            <center><img src="https://openclipart.org/download/247324/abstract-user-flat-1.svg" 
-              width={50} 
-              height={50} 
-              alt="usericon"/>
-              <p>{buddies[2] !== undefined ? buddies[2].name : 'NAME'}</p></center>
-            <p>Age: {buddies[2] !== undefined ? buddies[2].age : ''}</p>
-            <p>Interests: {buddies[2] !== undefined ? buddies[2].interests : ''}</p>
-            <p>Travel Spots: {buddies[2] !== undefined ? buddies[2].travel_spots : ''}</p>
-            <p>Hobbies: {buddies[2] !== undefined ? buddies[2].hobbies : ''}</p>
-            <p>Working Out: {buddies[2] !== undefined ? String(buddies[2].working_out) : ''}</p>
-            <p>Distance: Within 25 miles</p>
-          </div>
-        </div>
+        {showMatches && <RevealUserMatches />}
       </div>
     </main>
   );
